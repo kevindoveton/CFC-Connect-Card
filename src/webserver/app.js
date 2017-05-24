13855d73aebcbe7b7a36cac07aaad5154bf4aef3
kevindoveton <kevindoveton@me.com>
2017-05-24 09:46:52 +0930

    added basic api functionality

--- src/webserver/app.js
+++ src/webserver/app.js
@@ -5,13 +5,15 @@
 const express = require('express');
 const app = express();
 
+// express config
+// -
 app.disable('x-powered-by');
 app.set('view engine', 'pug');
 
 // routes
 // -----------------
 const api = require('./routes/api');
-app.use('/api', api);
+app.use('/api/v1', api);
 
 const cp = require('./routes/cp');
 app.use('/cp', cp);
--- src/webserver/routes/api.js
+++ src/webserver/routes/api.js
@@ -2,11 +2,43 @@
 
 const router = require('express').Router();
 const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
+mongoose.Promise = global.Promise;
+
+
+mongoose.connect('mongodb://localhost/connect-cards');
+const CARD_SCHEMA = mongoose.Schema({
+	intent: String,
+	firstName: String,
+	lastName: String
+});
+const Card = mongoose.model('Card', CARD_SCHEMA);
 
 router.use(bodyParser.json());
 
 router.get('/', (req, res) => {
 	return res.send('api');
-})
+});
+
+router.get('/cards', (req, res) => {
+	Card.find((err, cards) => {
+		if (err) {
+			console.log(err);
+			return res.status(500).send();
+		}
+		return res.status(200).send(JSON.stringify(cards));
+	})
+});
+
+router.post('/cards', (req, res) => {
+	console.log(req.body);
+	const c = new Card({intent: req.body.intent});
+	c.save((err, card) => {
+		if (err) {
+			return res.status(500).send();
+		}
+		return res.status(201).send();
+	});
+});
 
 module.exports = router;
