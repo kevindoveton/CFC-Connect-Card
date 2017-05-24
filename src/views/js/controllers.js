85ccaf3872ae6d94819e88c58058d6b5a3443d15
kevindoveton <kevindoveton@me.com>
2017-05-24 22:31:48 +0930

    add loader

--- src/views/js/controllers.js
+++ src/views/js/controllers.js
@@ -3,8 +3,14 @@
 /* Controllers */
 angular.module('cfcConnect.controllers', []).controller('AppCtrl', function ($scope, $rootScope, $http, $templateCache) {
 
-	// $rootScope.$on('$stateChangeStart', function(event, toState, toParams, fromState, fromParams){ 		
+	$rootScope.$on('$stateChangeStart', function(event, toState, toParams, fromState, fromParams){ 		
 		// state change
-	// })
+		$scope.loading = true;
+		console.log('start')
+	})
+
+	$rootScope.$on('$stateChangeSuccess', function(event, toState, toParams, fromState, fromParams){ 
+		$scope.loading = false;
+	});
 	
 });
